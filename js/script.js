2c3aa4ad6d743c85827b9fa5dfadb979001330d1
Joe-Valente <112267629+Joe-Valente@users.noreply.github.com>
2022-11-07 22:03:18 -0600

    finished js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 function init(){    
-function click(){
-    var input= document.getElementById('entryinput').ariaValueMax;
+function IOFunction(){
+  var input= document.getElementById('entryinput').value;
+  var output= document.getElementById('textoutput');
+  output.innerHTML = input;
+  alert("Joe Valente: " + input);
+  }
+  var Button = document.getElementById('entrybutton');
+  Button.addEventListener('click', IOFunction);
 }
 window.addEventListener('load', init);
